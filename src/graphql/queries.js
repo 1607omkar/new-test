cf6dca87f5456206b5aaebe69706105635cf1c0e
EC2 Default User <ec2-user@ip-172-31-40-121.ec2.internal>
2020-01-09 15:58:42 +0000

    test

---
+++ src/graphql/queries.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getExample = `query GetExample($id: String!) {
+  getExample(id: $id) {
+    json
+  }
+}
+`;
